a58df4ef195db00dccf69b6e1d22834c40daa83d
Mikhail <mikhail.id.123@gmail.com>
2022-02-11 20:59:35 +0400

    Project skeleton

---
+++ app/src/main/java/com/github/vehicledashboard/presentation/ui/AppActivity.kt
@@ -0,0 +1,35 @@
+package com.github.vehicledashboard.presentation.ui
+
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import androidx.appcompat.app.AppCompatActivity
+import com.github.vehicledashboard.databinding.AppActivityBinding
+
+class AppActivity : AppCompatActivity() {
+
+    private lateinit var binding: AppActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = AppActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (Build.VERSION.SDK_INT >= 30) {
+            binding.fullscreenContent.windowInsetsController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+        } else {
+            // Note that some of these constants are new as of API 16 (Jelly Bean)
+            // and API 19 (KitKat). It is safe to use them, as they are inlined
+            // at compile-time and do nothing on earlier devices.
+            binding.fullscreenContent.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                        View.SYSTEM_UI_FLAG_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        }
+    }
+}
